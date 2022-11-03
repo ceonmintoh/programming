0d111bc97dc0453d33a147a84a81dcc71466fa0a
NELSON JOSEPH MINTOH <35198246+nmintoh@users.noreply.github.com>
2022-11-03 11:40:44 +0100

    Login Create

---
+++ nelsonict/login.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Nelson ICT Services website">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="generator" content="Hugo 0.101.0">
+        <title>Nelsonict Services</title>
+    
+        <!--link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/headers/"-->
+    
+        
+    
+        
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+        <style>
+          .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+          }
+    
+          @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+              font-size: 3.5rem;
+            }
+          }
+    
+          .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+          }
+    
+          .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+          }
+    
+          .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+          }
+    
+          .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+          }
+    
+          .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+          }
+        </style>
+    
+        
+        <!-- Custom styles for this template -->
+        <link href="css/headers.css" rel="stylesheet">
+      </head>
+    <body>
+        <!--Header-->
+        <div class="bg-dark text-white">
+            <header class="d-flex flex-wrap justify-content-center py-2 px-5 mb-4 border-bottom">
+              <a href="https://nelsonict.org.ng" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+                <img class="me-2" src="img/farvico.ico" width="40" height="32"/>
+                <span class="fs-4 text-success fw-bold">NELSONICT SERVICES</span>
+              </a>
+        
+              <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">E-Learning</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Downloads</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+              </ul>
+            </header>
+        </div>
+        <!-- Sign in-->  
+    <main class="form-signin w-100 m-auto">
+        <form>
+            <img class="mb-4" src="farvico.ico" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted"><a href="#">Forgot Password</a></p>
+        </form>
+    </main>
+
+
+<!--Footer Codes-->
+     <div class="bg-dark text-white">
+        <footer class="py-5 px-5">
+        <div class="row">
+            <div class="col-6 col-md-2 mb-3">
+            <h5>Courses</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Computer Application</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Mobile Development</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Penetration Testing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Blockchain / Web3</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Website Development</a></li>
+            </ul>
+            </div>
+    
+            <div class="col-6 col-md-2 mb-3">
+            <h5>Services</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Consultation</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Data Entry</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Certification</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Troubleshooting</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Development</a></li>
+            </ul>
+            </div>
+    
+            <div class="col-6 col-md-2 mb-3">
+            <h5>Downloads</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Books</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Software</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Templates</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Videos</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
+            </ul>
+            </div>
+    
+            <div class="col-md-5 offset-md-1 mb-3">
+            <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                <label for="newsletter1" class="visually-hidden">Email address</label>
+                <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                <button class="btn btn-primary" type="button">Subscribe</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>&copy; 2018 NELSON ICT SERVICES GEMBU. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24" ><image href="img/facebook.svg" hieght="24" width="24" style="color:#e46815;"/></svg></a></li>
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><image href="img/twitter.svg" hieght="24" width="24"/></svg></a></li>
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><image href="img/instagram.svg" hieght="24" width="24"/></svg></a></li>
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><image href="img/telegram.svg" hieght="24" width="24"/></svg></a></li>
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><image href="img/whatsapp.svg" hieght="24" width="24"/></svg></a></li>
+            <li class="ms-3 bg-light"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><image href="img/github.svg" hieght="24" width="24"/></svg></a></li>
+            </ul>
+        </div>
+        </footer>
+    </div>
+    </body>
+</html>
